93fc021f5653fe290d08a230eb205f85c3b12279
Luis David Quillas León <a20216150@pucp.edu.pe>
2026-09-17 20:09:09 -0500

    primer avance

---
+++ src/main/java/org/example/laboratorio4_20216150/Laboratorio420216150Application.java
@@ -0,0 +1,13 @@
+package org.example.laboratorio4_20216150;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio420216150Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Laboratorio420216150Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/laboratorio4_20216150/Laboratorio420216150ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.laboratorio4_20216150;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorio420216150ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
